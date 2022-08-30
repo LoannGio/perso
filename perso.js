03082fd7f89d8f75d09a668bf006c31606e76bc0
LoannGio <loann.giovannangeli@u-bordeaux.fr>
2022-08-30 13:25:56 +0200

    first commit

---
+++ perso.js
@@ -0,0 +1,25 @@
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+// Modal Image Gallery
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
+
+function linkDOI() {
+    let dois = document.getElementsByClassName("doi");
+    for(let i = 0; i < dois.length; i++){
+        dois[i].href = "https://doi.org/" + dois[i].text;
+    }
+}
